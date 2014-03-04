d878ba904fa8ee844ed330bc6bbe3b04e9464a32
Bob Lail <bob.lail@cph.org>
2014-03-04 14:53:54 -0600

    [fix] Stopped breaking when there's only one issues (5m)

--- app/controllers/houston/itsm/issues_controller.rb
+++ app/controllers/houston/itsm/issues_controller.rb
@@ -3,12 +3,12 @@ require 'ntlm/http'
 class Houston::Itsm::IssuesController < ApplicationController
   
   def index
-    http = Net::HTTP.start("ecphhelpertest", 80)
+    http = Net::HTTP.start("ecphhelper", 80)
     req = Net::HTTP::Get.new("/ITSM.asmx/GetOpenCallsEmergingProducts")
     req.ntlm_auth("Houston", "cph.pri", "gKfub6mFy9BHDs6")
     response = http.request(req)
     issues = Hash.from_xml(response.body).fetch("ArrayOfOpenCallData").fetch("OpenCallData", [])
-    @issues = issues.map do |issue|
+    @issues = Array.wrap(issues).map do |issue|
       url = Nokogiri::HTML::fragment(issue["CallDetailLink"]).children.first[:href]
       user = User.find_by_email_address(issue["AssignedToEmailAddress"])
       Issue.new(issue["Summary"], url, issue["AssignedToEmailAddress"], user)
