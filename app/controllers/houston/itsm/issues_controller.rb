d88429edaa0b86049b8c2cd0fe594cb317e0ae86
Bob Lail <bob.lail@cph.org>
2014-03-18 09:10:29 -0500

    [fix] Released connection before requesting ITSMs so that it isn't tied up during the rest of the request (20m)

--- app/controllers/houston/itsm/issues_controller.rb
+++ app/controllers/houston/itsm/issues_controller.rb
@@ -3,15 +3,23 @@ require 'ntlm/http'
 class Houston::Itsm::IssuesController < ApplicationController
   
   def index
-    http = Net::HTTP.start("ecphhelper", 80)
-    req = Net::HTTP::Get.new("/ITSM.asmx/GetOpenCallsEmergingProducts")
-    req.ntlm_auth("Houston", "cph.pri", "gKfub6mFy9BHDs6")
-    response = http.request(req)
-    issues = Hash.from_xml(response.body).fetch("ArrayOfOpenCallData").fetch("OpenCallData", [])
-    @issues = Array.wrap(issues).map do |issue|
-      url = Nokogiri::HTML::fragment(issue["CallDetailLink"]).children.first[:href]
-      user = User.find_by_email_address(issue["AssignedToEmailAddress"])
-      Issue.new(issue["Summary"], url, issue["AssignedToEmailAddress"], user)
+    users_by_email = benchmark("\e[33mEager-load users\e[0m") do
+      User.all.each_with_object({}) { |user, map| user.email_addresses.each { |email| map[email.downcase] = user } }
+    end
+    
+    ActiveRecord::Base.clear_active_connections! unless env.key?('rack.test')
+    
+    benchmark("\e[33mFetch ITSMs\e[0m") do
+      http = Net::HTTP.start("ecphhelper", 80)
+      req = Net::HTTP::Get.new("/ITSM.asmx/GetOpenCallsEmergingProducts")
+      req.ntlm_auth("Houston", "cph.pri", "gKfub6mFy9BHDs6")
+      response = http.request(req)
+      issues = Hash.from_xml(response.body).fetch("ArrayOfOpenCallData").fetch("OpenCallData", [])
+      @issues = Array.wrap(issues).map do |issue|
+        url = Nokogiri::HTML::fragment(issue["CallDetailLink"]).children.first[:href]
+        user = users_by_email[issue["AssignedToEmailAddress"].try(:downcase)]
+        Issue.new(issue["Summary"], url, issue["AssignedToEmailAddress"], user)
+      end
     end
     
     render partial: "houston/itsm/issues/fires" if request.xhr?
