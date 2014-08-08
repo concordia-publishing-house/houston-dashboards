85b7a117c0723bc0c1a6feb805d4c675cebe15dd
Bob Lail <bob.lail@cph.org>
2014-08-07 19:39:44 -0500

    [refactor] Extracted the fetch-ITSM logic to a model (10m)

--- app/controllers/houston/itsm/issues_controller.rb
+++ app/controllers/houston/itsm/issues_controller.rb
@@ -1,41 +1,21 @@
-require 'ntlm/http'
-
 class Houston::Itsm::IssuesController < ApplicationController
   layout "dashboard"
   
   def index
-    users_by_email = benchmark("\e[33mEager-load users\e[0m") do
-      User.all.each_with_object({}) { |user, map| user.email_addresses.each { |email| map[email.downcase] = user } }
+    benchmark("\e[33mFetch ITSMs\e[0m") do
+      @issues = Houston::Itsm::Issue.all
     end
     
-    ActiveRecord::Base.clear_active_connections! unless env.key?('rack.test')
-    
-    benchmark("\e[33mFetch ITSMs\e[0m") do
-      http = Net::HTTP.start("ecphhelper", 80)
-      req = Net::HTTP::Get.new("/ITSM.asmx/GetOpenCallsEmergingProducts")
-      req.ntlm_auth("Houston", "cph.pri", "gKfub6mFy9BHDs6")
-      response = http.request(req)
-      @issues = parse_issues(response.body).map do |issue|
-        url = Nokogiri::HTML::fragment(issue["CallDetailLink"]).children.first[:href]
-        user = users_by_email[issue["AssignedToEmailAddress"].try(:downcase)]
-        Issue.new(issue["Summary"], url, issue["AssignedToEmailAddress"], user)
+    benchmark("\e[33mLoad Users\e[0m") do
+      users_by_email = User.all.each_with_object({}) { |user, map|
+        user.email_addresses.each { |email| map[email.downcase] = user } }
+      
+      @issues.each do |issue|
+        issue.assigned_to_user = users_by_email[issue.assigned_to_email]
       end
     end
     
     render partial: "houston/itsm/issues/fires" if request.xhr?
   end
-  
-  def parse_issues(xml)
-    Array.wrap(
-      Hash.from_xml(xml)
-        .fetch("ArrayOfOpenCallData", {})
-        .fetch("OpenCallData", []))
-  rescue REXML::ParseException # malformed response upstream
-    Rails.logger.error "\e[31;1m#{$!.class}\e[0;31m: #{$!.message}"
-    []
-  end
-  
-  
-  Issue = Struct.new(:summary, :url, :assigned_to_email, :assigned_to_user)
   
 end
---
+++ app/models/houston/itsm/issue.rb
@@ -0,0 +1,34 @@
+require "ntlm/http"
+
+module Houston
+  module Itsm
+    class Issue < Struct.new(:summary, :url, :assigned_to_email, :assigned_to_user)
+      
+      
+      def self.all
+        http = Net::HTTP.start("ecphhelper", 80)
+        req = Net::HTTP::Get.new("/ITSM.asmx/GetOpenCallsEmergingProducts")
+        req.ntlm_auth("Houston", "cph.pri", "gKfub6mFy9BHDs6")
+        response = http.request(req)
+        parse_issues(response.body).map do |issue|
+          url = Nokogiri::HTML::fragment(issue["CallDetailLink"]).children.first[:href]
+          self.new(issue["Summary"], url, issue["AssignedToEmailAddress"].try(:downcase))
+        end
+      end
+      
+      
+    private
+      
+      def self.parse_issues(xml)
+        Array.wrap(
+          Hash.from_xml(xml)
+            .fetch("ArrayOfOpenCallData", {})
+            .fetch("OpenCallData", []))
+      rescue REXML::ParseException # malformed response upstream
+        Rails.logger.error "\e[31;1m#{$!.class}\e[0;31m: #{$!.message}"
+        []
+      end
+      
+    end
+  end
+end
