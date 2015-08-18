2eda443f5b36913a3c2e94b2ab0345bdc0b99156
Bob Lail <bob.lail@cph.org>
2015-08-17 21:07:51 -0500

    [feature] Added dashboards for showing recent and upcoming changes to Members, Unite, and Ledger (2h)

---
+++ app/controllers/houston/dashboards/releases_controller.rb
@@ -0,0 +1,48 @@
+class Houston::Dashboards::ReleasesController < ApplicationController
+  layout "houston/dashboards/dashboard"
+  
+  def index
+    @title = "Changes"
+    
+    @upcoming_changes = %w{members unite ledger}.flat_map do |slug|
+      project = Project.find_by_slug slug
+      branches = project.repo.branches
+      master, beta = branches.values_at "master", "beta"
+      commits = project.commits.between(master, beta)
+      release = Release.new(project: project)
+      commits.map { |commit| ReleaseChange.from_commit(release, commit) }
+        .reject { |change| change.tag.nil? }
+    end
+    
+    projects = Project.where(slug: %w{members unite ledger})
+    releases = Release.where(project_id: projects.map(&:id)).limit(20)
+    @recent_changes = releases.flat_map(&:release_changes).take(12)
+  end
+  
+  def upcoming
+    @title ="Upcoming"
+    
+    @changes = %w{members unite ledger}.flat_map do |slug|
+      project = Project.find_by_slug slug
+      branches = project.repo.branches
+      master, beta = branches.values_at "master", "beta"
+      commits = project.commits.between(master, beta)
+      release = Release.new(project: project)
+      commits.map { |commit| ReleaseChange.from_commit(release, commit) }
+        .reject { |change| change.tag.nil? }
+    end
+    
+    render partial: "houston/dashboards/issues/changes" if request.xhr?
+  end
+  
+  def recent
+    @title ="Recent"
+    
+    projects = Project.where(slug: %w{members unite ledger})
+    releases = Release.where(project_id: projects.map(&:id)).limit(20)
+    @changes = releases.flat_map(&:release_changes).take(15)
+    
+    render partial: "houston/dashboards/issues/changes" if request.xhr?
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Houston::Dashboards::Engine.routes.draw do
   
   get "issues", :to => "issues#index"
+  get "changes", :to => "releases#index"
+  get "recent", :to => "releases#recent"
+  get "upcoming", :to => "releases#upcoming"
   
 end
