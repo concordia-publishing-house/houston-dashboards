341210e35760d96a39137505dfed2984a3482c3c
Bob Lail <bob.lail@cph.org>
2015-10-10 11:32:23 -0500

    [skip] Namespaced routes (1m)

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Houston::Dashboards::Engine.routes.draw do
 
-  get "issues", :to => "issues#index"
-  get "releases", :to => "releases#index"
-  get "recent", :to => "releases#recent"
-  get "upcoming", :to => "releases#upcoming"
+  scope "dashboards" do
+    get "issues", :to => "issues#index"
+    get "releases", :to => "releases#index"
+    get "recent", :to => "releases#recent"
+    get "upcoming", :to => "releases#upcoming"
+  end
 
 end
