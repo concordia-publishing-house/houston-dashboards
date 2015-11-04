f3a1ad65599be3132f9b8471db845179bcc738eb
Chase Clettenberg <chase.clettenberg@cph.org>
2015-11-04 08:41:43 -0600

    [skip] New dashboard to see what is on staging and what needs testing (3h)

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Houston::Dashboards::Engine.routes.draw do
     get "releases", :to => "releases#index"
     get "recent", :to => "releases#recent"
     get "upcoming", :to => "releases#upcoming"
+    get "staging", :to => "staging#index"
   end
 
 end
