09de2a44da5f0424f71ca3645783fab5c22bbd6d
Bob Lail <bob.lailfamily@gmail.com>
2016-01-09 21:32:11 -0600

    [feature] Added a Kanban dashboard for pull requests (1.5h)

---
+++ app/controllers/houston/dashboards/pulls_controller.rb
@@ -0,0 +1,31 @@
+class Houston::Dashboards::PullsController < ApplicationController
+  layout "houston/dashboards/dashboard"
+
+  def index
+    @queues = { wip: [], review_needed: [], test_pending: [], test_needed: [], depoy_needed: [] }
+    pulls = Github::PullRequest.without_labels("archived", "experimental").order(created_at: :desc)
+    pulls.each do |pull|
+      if pull.labeled?("wip")
+        @queues[:wip].push pull
+        next
+      end
+
+      unless pull.labeled_any?("review-pass", "review-hold")
+        @queues[:review_needed].push pull
+      end
+
+      if pull.labeled?("review-pass") && pull.labeled_any?("test-pass", "no-test")
+        @queues[:deploy_needed].push pull
+      elsif pull.labeled?("test-needed", "on-staging")
+        @queues[:test_needed].push pull
+      elsif pull.labeled?("test-needed") && !pull.labeled?("on-staging")
+        @queues[:test_pending].push pull
+      else
+        @queues[:wip].push pull
+      end
+    end
+
+    render partial: "houston/dashboards/pulls/pulls" if request.xhr?
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Houston::Dashboards::Engine.routes.draw do
     get "recent", :to => "releases#recent"
     get "upcoming", :to => "releases#upcoming"
     get "staging", :to => "staging#index"
+    get "pulls", :to => "pulls#index"
   end
 
 end
