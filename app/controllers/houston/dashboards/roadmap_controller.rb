63a0fb07e80ec8ce57c6a34b5e8a304e4b62956d
Bob Lail <bob.lailfamily@gmail.com>
2016-02-07 21:19:51 -0600

    [skip] Updated the Roadmaps Dashboard to work with houston-roadmaps (5m)

--- app/controllers/houston/dashboards/roadmap_controller.rb
+++ app/controllers/houston/dashboards/roadmap_controller.rb
@@ -4,7 +4,7 @@ class Houston::Dashboards::RoadmapController < ApplicationController
   def index
     today = Date.today
     @range = 6.months.before(today)..6.months.after(today)
-    @milestones = Milestone.during(@range)
+    @milestones = RoadmapMilestone.during(@range)
     @show_today = params[:today] != "false"
 
     @title = "Roadmap"
@@ -12,12 +12,12 @@ class Houston::Dashboards::RoadmapController < ApplicationController
     @sprint = Sprint.find_by_id(params[:sprint_id]) || Sprint.current || Sprint.create!
 
     respond_to do |format|
-      format.html { render layout: "houston/roadmap/dashboard" }
+      format.html { render layout: "houston/roadmaps/dashboard" }
       format.json { render json: {
         start: @sprint.start_date,
         tasks: SprintTaskPresenter.new(@sprint).as_json,
         range: {start: @range.begin, end: @range.end},
-        milestones: Houston::Roadmap::MilestonePresenter.new(@milestones) } }
+        milestones: Houston::Roadmaps::RoadmapMilestonePresenter.new(@milestones) } }
     end
   end
 
