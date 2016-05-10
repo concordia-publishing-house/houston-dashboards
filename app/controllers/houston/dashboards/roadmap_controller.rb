b190209d4f08f8f98a2431ee7fb9b3b82086810d
Bob Lail <bob.lailfamily@gmail.com>
2016-05-09 21:01:40 -0500

    [skip] Added the ability to specify a roadmap to show (1m)

--- app/controllers/houston/dashboards/roadmap_controller.rb
+++ app/controllers/houston/dashboards/roadmap_controller.rb
@@ -4,7 +4,8 @@ class Houston::Dashboards::RoadmapController < ApplicationController
   def index
     today = Date.today
     @range = 6.months.before(today)..6.months.after(today)
-    @milestones = RoadmapMilestone.during(@range)
+    @milestones = RoadmapMilestone.during(@range).preload(:milestone => :project)
+    @milestones = @milestones.where(roadmap_id: params[:roadmap_id]) if params.key?(:roadmap_id)
     @show_today = params[:today] != "false"
 
     @title = "Roadmap"
